85b58872216dc09268493b96d8ea6451e3d4a0e1
Aryan Keshri <aryankeshri1991@gmail.com>
2025-05-02 21:28:27 +0530

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -21,3 +21,26 @@ try:
   print(obj2)
 except Exception as error:
   print(error)
+
+
+# Singlton Class 
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if Singleton.__instance is not None:
+            raise Exception("Singleton cannot be instantiated more than once!")
+        else:
+            Singleton.__instance = self
+
+    @staticmethod
+    def get_instance():
+        if Singleton.__instance is None:
+            Singleton()
+        return Singleton.__instance
+
+s1 = Singleton.get_instance()
+s2 = Singleton.get_instance()
+print(s1 is s2)  # Output: True
+
+s3 = Singleton() # This will raise an error
